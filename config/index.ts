a184ea0e04d47be94083521ff2585ae6bcc116c6
morispolanco <morispolanco@gmail.com>
2023-07-26 15:53:59 -0600

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,17 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '8af2c01e-bf0a-4429-984c-33d7f5ccf029'
+export const API_KEY = 'app-OZw6qix4wsjQl4MUTmlpEukZ'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'Leybotgt',
+  description: 'Consulta de las leyes de Guatemala',
+  copyright: '2023 Leybot',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'es-ES',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = 'I want you to act as an expert in the legal syystem of Guatemala.'
 
 export const API_PREFIX = '/api'
 
